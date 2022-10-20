d0207b1e70fd9bb213081af9b1979656ab136a66
CelsoAlmeida <celso.almeida.leite@hotmail.com>
2022-10-20 20:50:58 -0300

    fase1: update framework

--- src/modules/framework.ts
+++ src/modules/framework.ts
@@ -1,5 +1,8 @@
+import { format } from 'date-fns';
+
 export class Framework {
     constructor(){
-        console.log('[Framework]')
+        let date = `${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`;
+        console.log(`[Framework] ${date}`)
     }
 }
